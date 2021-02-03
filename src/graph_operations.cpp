843fd074fb9670b6b01513b76be27fe572f7eae7
Adam Tamargo <tamargoadam@gmail.com>
2021-02-03 14:16:19 -0500

    implemented adj list graph

--- src/graph_operations.cpp
+++ src/graph_operations.cpp
@@ -7,25 +7,21 @@ using namespace std;
 template<class T>
 class Graph {
   public:
-
-    int numNodes;
-    unordered_map<T*, vector<T *>> adjList;
-
-    Graph() {
-      numNodes=0;
+    int size() {
+      return adjList.size();
     }
 
-    void add_edge_undir(T &n1, T &n2) {
-      add_edge(n1, n2);
-      add_edge(n2, n1);
+    void add_edge(T &n1, T &n2) {
+      adjList[&n1].push_back(&n2);
+      adjList[&n2].push_back(&n1);
     }
 
     void print_graph()
     {
       for (auto const& pair: adjList) {
           cout << *pair.first << ": ";
-          for (auto const& val: pair.second) {
-            cout << *val << " ";
+          for(int i=0; i < pair.second.size(); i++) {
+            cout << *pair.second.at(i) << " ";
           }
           cout << endl;
       }
@@ -39,21 +35,17 @@ class Graph {
     // }
 
   private:
-    void add_edge(T &n1, T &n2) {
-      if (adjList.find(&n1) != adjList.end()) {
-        // adjList.insert(make_pair(&n1, vector<T *>()));
-        adjList[&n1] = vector<T *>();
-      }
-      adjList[&n1].push_back(&n2);
-    }
+    unordered_map<T*, vector<T *>> adjList;
 };
 
 
 int main() {
   Graph<int> g = Graph<int>();
   vector<int> v = {1,2,3,4,5,6,7,8,9,10};
-  g.add_edge_undir(v[0], v[1]);
-  g.add_edge_undir(v[0], v[5]);
+  g.add_edge(v[0], v[1]);
+  g.add_edge(v[0], v[5]);
+  g.add_edge(v[1], v[9]);
+  g.add_edge(v[5], v[2]);
   g.print_graph();
   return 0;
 }
