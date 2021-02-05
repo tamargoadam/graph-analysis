5b2415320bb7511ec77c98aeca8547a114374184
Adam Tamargo <tamargoadam@gmail.com>
2021-02-05 15:35:43 -0500

    moved main to simulated_test.cpp (still needs to be expanded for testing all graph topologies)

--- src/graph_simulator.cpp
+++ src/graph_simulator.cpp
@@ -56,29 +56,3 @@ Graph<int> heap(int n) {
 
     return result;
 }
-
-int main() {
-    Graph<int> g = cycle_graph(10);
-    g.print_graph();
-
-    cout << endl;
-
-    vector<vector<int*>> comps = g.connected_components();
-    for (int i = 0; i < comps.size(); i++) {
-        for (int j = 0; j < comps[i].size(); j++)
-        cout << *comps[i][j] << " ";
-        cout << endl;
-    }
-    cout << endl;
-
-    vector<int*> cycle = g.one_cycle();
-    for(int i=0; i<cycle.size(); i++)
-        cout << *cycle[i] << " ";
-    cout << endl << endl;
-
-    vector<int*> shp = g.shortest_path(2, 7);
-    for(int i=0; i<shp.size(); i++)
-        cout << *shp[i] << " ";
-
-    return 0;
-}
